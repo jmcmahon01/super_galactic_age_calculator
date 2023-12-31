de01a906a2a8f855f2d002fea0375d2d01fe45c8
jmcmahon01 <joseph.mcmahon2d9@gmail.com>
2023-12-30 22:09:40 -0800

    add mercury button in index.html and ui logic in index.js

--- src/index.js
+++ src/index.js
@@ -3,21 +3,51 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import './css/styles.css';
 import User from '../src/js/age-calculator.js';
 
-function handleUserForm() {
-  event.preventDefault();
-  document.querySelector('#response').innerText = null;
-  const name = document.querySelector('#inputName').value;
-  const age = parseInt(document.querySelector('#inputAge').value);
-  const user = new User(name, age);
-  const response1 = user.name;
-  const response2 = user.age;
-  const pTag = document.createElement("p");
-  pTag.append(`Your name: ${response1}. Your Age: ${response2}.`);
-  document.querySelector('#response').append(pTag);
-}
 window.addEventListener("load", function () {
-  document.querySelector("#userForm").addEventListener("submit", handleUserForm);
+  const userForm = document.querySelector("#userForm");
+  const responseContainer = document.querySelector('#response');
+
+  let userInstance = null;
+
+  function handleUserForm(event) {
+    event.preventDefault();
+    responseContainer.innerText = null;
+
+    const name = document.querySelector('#inputName').value;
+    const age = parseInt(document.querySelector('#inputAge').value);
+
+    userInstance = new User(name, age);
+
+    const response1 = name;
+    const response2 = age;
+    const pTag = document.createElement("p");
+    pTag.append(`Your name: ${response1}. Your Age: ${response2}.`);
+    responseContainer.append(pTag);
+  }
+
+  userForm.addEventListener("submit", handleUserForm);
+
+  const mercuryButton = document.getElementById("mercury");
+  const mercResponse = document.getElementById("mercResponse");
+  const displayMercuryAge = document.getElementById("displayAge");
+
+  mercuryButton.addEventListener("click", function () {
+    if (!userInstance) {
+      alert('Please submit the form first.');
+      return;
+    }
+
+    const mercuryAge = userInstance.calculateMercuryAge();
+
+    displayMercuryAge.innerText = `${mercuryAge} years`;
+
+    mercResponse.classList.remove("hidden");
+  });
 });
+
+
+
+
 
 
 
